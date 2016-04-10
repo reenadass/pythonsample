ea52c8dc4863dd3851216ecd9c19a499e0df392c
rdass <reena.dass@gmail.com>
2016-04-09 20:32:19 -0700

    this is my first commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import sys
+sys.path.append("~/pythonProjects/sandbox2")
+
+
+from sandbox2.hello1 import printHello
+
+printHello()
